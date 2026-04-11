bf060440a3f148bc9ea2d15931123f03702e6cbc
Eugene Li <liyujun@m1air.local>
2026-04-11 12:48:12 +0800

    refactor: rename config dir to .config

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ CONFIG_HOME="$HOME/.config"
 [ -d "$CONFIG_HOME" ] || mkdir $CONFIG_HOME
 ln -snf $BASE_DIR/bashrc $HOME/.bashrc && source $HOME/.bashrc
 ln -snf $BASE_DIR/inputrc $HOME/.inputrc
-ln -snf $BASE_DIR/config/nvim $CONFIG_HOME/nvim
-ln -snf $BASE_DIR/config/starship/starship.toml $CONFIG_HOME/starship.toml
-ln -snf $BASE_DIR/config/zellij $CONFIG_HOME/zellij
-ln -snf $BASE_DIR/config/alacritty $CONFIG_HOME/alacritty
-ln -snf $BASE_DIR/config/lazygit $CONFIG_HOME/lazygit
+ln -snf $BASE_DIR/.config/nvim $CONFIG_HOME/nvim
+ln -snf $BASE_DIR/.config/starship/starship.toml $CONFIG_HOME/starship.toml
+ln -snf $BASE_DIR/.config/zellij $CONFIG_HOME/zellij
+ln -snf $BASE_DIR/.config/alacritty $CONFIG_HOME/alacritty
+ln -snf $BASE_DIR/.config/lazygit $CONFIG_HOME/lazygit
+ln -snf $BASE_DIR/.config/mise $CONFIG_HOME/mise
