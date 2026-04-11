3d80cd20c640eceb63b805d0af014fde235e4373
Eugene Li <liyujun@m1air.local>
2026-04-11 12:48:00 +0800

    refactor(debian): new debian setup

--- debian.sh
+++ debian.sh
@@ -24,8 +24,8 @@ install_lazygit() {
     echo "Installing Lazygit"
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    tar xf lazygit.tar.gz lazygit
-    sudo install lazygit -D -t /usr/local/bin/
+    tar xf lazygit.tar.gz -C /tmp/lazygit
+    sudo install /tmp/lazygit -D -t /usr/local/bin/
     echo "Lazygit Installed"
   }
 }
